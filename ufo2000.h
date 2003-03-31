f56da6182c0fdf02ff8409bd5668235721eaf873
serge <serge>
2003-03-31 01:58:27 +0000

    Added unicode 8x8 font (latin1 and cyrillic) and also fonts from russian version of xcom1 to datafile
    git-svn-id: https://ufo2000.svn.sourceforge.net/svnroot/ufo2000/ufo2k/0.3-devel@109 80a73507-52ba-0310-bdc1-b9b4a1856fab

--- ufo2000.h
+++ ufo2000.h
@@ -1,6 +1,6 @@
-/* Allegro datafile object indexes, produced by grabber v4.0.1, MSVC.s */
-/* Datafile: h:\ufo2000\dat\ufo2000.dat */
-/* Date: Sun Jul 28 22:24:23 2002 */
+/* Allegro datafile object indexes, produced by dat v4.0.1, MSVC.s */
+/* Datafile: ufo2000.dat */
+/* Date: Mon Mar 31 04:47:31 2003 */
 /* Do not hand edit! */
 
 #define BWD_ABOUT                        0        /* BWD  */
@@ -9,14 +9,17 @@
 #define BWD_INET                         3        /* BWD  */
 #define BWD_IPX                          4        /* BWD  */
 #define BWD_MODEM                        5        /* BWD  */
-#define DAT_FONT_CYR                     6        /* FONT */
-#define DAT_GAMEPAL                      7        /* PAL  */
-#define DAT_KASTET                       8        /* BMP  */
-#define DAT_KNIFE                        9        /* BMP  */
-#define DAT_LOGO                         10       /* BMP  */
-#define DAT_MENUPAL                      11       /* PAL  */
-#define DAT_MOUSE                        12       /* BMP  */
-#define DAT_MOUSE2                       13       /* BMP  */
-#define DAT_SELECT                       14       /* BMP  */
-#define DAT_TEXT_BACK                    15       /* DATA */
+#define DAT_BIGLETS_CYR                  6        /* DATA */
+#define DAT_FONT_CYR                     7        /* FONT */
+#define DAT_GAMEPAL                      8        /* PAL  */
+#define DAT_KASTET                       9        /* BMP  */
+#define DAT_KNIFE                        10       /* BMP  */
+#define DAT_LOGO                         11       /* BMP  */
+#define DAT_MENUPAL                      12       /* PAL  */
+#define DAT_MOUSE                        13       /* BMP  */
+#define DAT_MOUSE2                       14       /* BMP  */
+#define DAT_SELECT                       15       /* BMP  */
+#define DAT_SMALLSET_CYR                 16       /* DATA */
+#define DAT_TEXT_BACK                    17       /* DATA */
+#define DAT_UNIFONT_TXT                  18       /* FONT */
 
