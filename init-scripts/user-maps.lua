28f53857e88ff8f890941e250c63a6f45bc55e7f
serge <serge>
2004-02-05 08:24:57 +0000

    New 'Airfield' terrain added (thanks to Twilight Owl)
    git-svn-id: https://ufo2000.svn.sourceforge.net/svnroot/ufo2000/trunk@329 80a73507-52ba-0310-bdc1-b9b4a1856fab

--- init-scripts/user-maps.lua
+++ init-scripts/user-maps.lua
@@ -110,8 +110,7 @@ AddXcomTerrain {
 		"$(tftd)/terrain/port01.*",
 		"$(tftd)/terrain/port02.*",
 		"$(xcom)/terrain/roads.*",
-		"$(xcom)/terrain/urbits.*",
-				
+		"$(xcom)/terrain/urbits.*"
 	},
 	Maps = {
 		"$(ufo2000)/newmaps/bagirov00.map",
@@ -130,5 +129,35 @@ AddXcomTerrain {
 		"$(ufo2000)/newmaps/bagirov13.map",
 		"$(ufo2000)/newmaps/bagirov14.map",
 		"$(ufo2000)/newmaps/bagirov15.map"
+	}
+}
+
+AddXcomTerrain {
+	Index = 204, Name = "Airfield",
+	Tiles =	{
+		"$(xcom)/terrain/blanks.*",
+		"$(tftd)/terrain/port01.*",
+		"$(tftd)/terrain/port02.*",
+		"$(xcom)/terrain/plane.*",
+		"$(xcom)/terrain/xbase2.*"
+	},
+	Maps = {
+		"$(ufo2000)/newmaps/air00.map",
+		"$(ufo2000)/newmaps/air01.map",
+		"$(ufo2000)/newmaps/air02.map",
+		"$(ufo2000)/newmaps/air03.map",
+		"$(ufo2000)/newmaps/air04.map",
+		"$(ufo2000)/newmaps/air05.map",
+		"$(ufo2000)/newmaps/air06.map",
+		"$(ufo2000)/newmaps/air07.map",
+		"$(ufo2000)/newmaps/air08.map",
+		"$(ufo2000)/newmaps/air09.map",
+		"$(ufo2000)/newmaps/air10.map",
+		"$(ufo2000)/newmaps/air11.map",
+		"$(ufo2000)/newmaps/air12.map",
+		"$(ufo2000)/newmaps/air13.map",
+		"$(ufo2000)/newmaps/air14.map",
+		"$(ufo2000)/newmaps/air15.map",
+		"$(ufo2000)/newmaps/air16.map"
 	}
 }
