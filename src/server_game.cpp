0cdff3e672b4f867dedc70e8b6db6cd37fc6e224
andrew <andrew>
2005-04-03 08:37:32 +0000

    Fix to make server part compatible with GCC 2.95.
    git-svn-id: https://ufo2000.svn.sourceforge.net/svnroot/ufo2000/trunk@788 80a73507-52ba-0310-bdc1-b9b4a1856fab

--- src/server_game.cpp
+++ src/server_game.cpp
@@ -19,6 +19,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
 
+#include <time.h>
 #include "server_game.h"
 #include "server_config.h"
 
