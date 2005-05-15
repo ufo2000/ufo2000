6c7d376bcbea5b4ee3d3c705b06a5a5da03e0263
serge <serge>
2005-05-15 14:50:16 +0000

    Information about errors added at server statistics page
    git-svn-id: https://ufo2000.svn.sourceforge.net/svnroot/ufo2000/trunk@830 80a73507-52ba-0310-bdc1-b9b4a1856fab

--- script/db2htmlreport.lua
+++ script/db2htmlreport.lua
@@ -28,10 +28,10 @@ db = sqlite3.open(arg[1])
 
 io.write("<br> <b>UFO2000 players rating table</b><br>")
 io.write("<table border=1>")
-io.write("<tr><td>name<td>games played<td>games won<td>ELO score<td>")
+io.write("<tr><td>name<td>games played<td>games won<td>ELO score")
 
-for html_row in db:cols([[
-	select "<tr><td>"||name||"<td>"||played||"<td>"||won||"<td>"||ifnull(elo_score,"")||"<td>" from 
+for name, played, won, elo_score in db:cols([[
+	select name, played, won, elo_score from 
 	( 
 	select name, 
 	(select count(*) from ufo2000_game_players p,ufo2000_games g 
@@ -45,17 +45,17 @@ for html_row in db:cols([[
 	order by elo_score desc 
 	]]) 
 do
-	io.write(html_row)
+	io.write("<tr><td>", name, "<td>", played ,"<td>", won, "<td>", elo_score, "\n")
 end
 
 io.write("</table>")
 
-io.write("<br> <b>UFO2000 played games statistics table</b><br>")
+io.write("<br> <b>UFO2000 recent games statistics</b><br>")
 io.write("<table border=1>\n")
-io.write("<tr><td>game<td>version<td>player1<td>player2<td>result<td>comment<td>\n")
+io.write("<tr><td>game<td>version<td>player1<td>player2<td>result<td>comment\n")
 
 for html_row in db:cols([[
-	select "<tr><td>"||id||"<td>"||ver||"<td>"||pl1||"<td>"||pl2||"<td>"||result||"<td>"||errors||"<td>" from 
+	select "<tr><td>"||id||"<td>"||ver||"<td>"||pl1||"<td>"||pl2||"<td>"||result||"<td>"||errors from 
 	(select id,ifnull(g.client_version, "") ver,p1.player pl1, p2.player pl2, case when g.result=1 then p1.player||" won" when g.result=2 then p2.player||" won" when g.result=3 then "draw" else "not finished" end result,ifnull(g.errors,"") errors 
 	from ufo2000_games g, ufo2000_game_players p1, ufo2000_game_players p2 
 	where g.id=p1.game and g.id=p2.game and p1.position=1 and p2.position=2) 
@@ -67,6 +67,19 @@ do
 end
 io.write("</table>")
 
-io.write(string.format("<br>report generated on %s<br>server log parsing performed for %.2f seconds", os.date(), os.clock()))
+io.write("<br> <b>UFO2000 bugs statistics table</b><br>")
+io.write("<table border=1>\n")
+io.write("<tr><td>game<td>version<td>error report\n")
+for game, version, error_report in db:cols([[
+    select d.game, g.client_version, d.value from 
+        ufo2000_debug_packets d, ufo2000_games g where 
+        d.game=g.id and (param="crash" or param="assert")
+        ]])
+do
+    io.write("<tr><td>", game, "<td>", version, "<td>", error_report, "\n")
+end
+io.write("</table>")
+
+io.write(string.format("<br>report generated on %s<br>time %.2f seconds", os.date(), os.clock()))
 io.write("</body></html>")
 db:close()
