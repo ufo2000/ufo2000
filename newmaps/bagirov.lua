5e99ea235f070b8aceb64a5d9c67a79076fb80a4
andrew <andrew>
2005-06-02 15:07:30 +0000

    Version bump
    git-svn-id: https://ufo2000.svn.sourceforge.net/svnroot/ufo2000/trunk@857 80a73507-52ba-0310-bdc1-b9b4a1856fab

--- newmaps/bagirov.lua
+++ newmaps/bagirov.lua
@@ -8,21 +8,21 @@ AddXcomTerrain {
 		"$(xcom)/terrain/urbits.*"
 	},
 	Maps = {
-		"$(ufo2000)/newmaps/bagirov00.map",
-		"$(ufo2000)/newmaps/bagirov01.map",
-		"$(ufo2000)/newmaps/bagirov02.map",
-		"$(ufo2000)/newmaps/bagirov03.map",
-		"$(ufo2000)/newmaps/bagirov04.map",
-		"$(ufo2000)/newmaps/bagirov05.map",
-		"$(ufo2000)/newmaps/bagirov06.map",
-		"$(ufo2000)/newmaps/bagirov07.map",
-		"$(ufo2000)/newmaps/bagirov08.map",
-		"$(ufo2000)/newmaps/bagirov09.map",
-		"$(ufo2000)/newmaps/bagirov10.map",
-		"$(ufo2000)/newmaps/bagirov11.map",
-		"$(ufo2000)/newmaps/bagirov12.map",
-		"$(ufo2000)/newmaps/bagirov13.map",
-		"$(ufo2000)/newmaps/bagirov14.map",
-		"$(ufo2000)/newmaps/bagirov15.map"
+		"$(ufo2000)/newmaps/bagirov/bagirov00.map",
+		"$(ufo2000)/newmaps/bagirov/bagirov01.map",
+		"$(ufo2000)/newmaps/bagirov/bagirov02.map",
+		"$(ufo2000)/newmaps/bagirov/bagirov03.map",
+		"$(ufo2000)/newmaps/bagirov/bagirov04.map",
+		"$(ufo2000)/newmaps/bagirov/bagirov05.map",
+		"$(ufo2000)/newmaps/bagirov/bagirov06.map",
+		"$(ufo2000)/newmaps/bagirov/bagirov07.map",
+		"$(ufo2000)/newmaps/bagirov/bagirov08.map",
+		"$(ufo2000)/newmaps/bagirov/bagirov09.map",
+		"$(ufo2000)/newmaps/bagirov/bagirov10.map",
+		"$(ufo2000)/newmaps/bagirov/bagirov11.map",
+		"$(ufo2000)/newmaps/bagirov/bagirov12.map",
+		"$(ufo2000)/newmaps/bagirov/bagirov13.map",
+		"$(ufo2000)/newmaps/bagirov/bagirov14.map",
+		"$(ufo2000)/newmaps/bagirov/bagirov15.map"
 	}
 }
