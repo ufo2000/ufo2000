19b7ef1045dd276db4919c42378e54850c3a2035
serge <serge>
2005-08-10 22:37:58 +0000

    Added sample civilian and ethereal appearances for 'chameleon' skin
    git-svn-id: https://ufo2000.svn.sourceforge.net/svnroot/ufo2000/trunk@896 80a73507-52ba-0310-bdc1-b9b4a1856fab

---
+++ extensions/unit-chameleon/civilian.lua
@@ -0,0 +1,19 @@
+local frames = pck_image_set("$(xcom)/units/civm.pck", 0, 8 * 9 + 3)
+
+local function sprite_renderer(img, dir, phase, info)
+    if info.is_falling then
+        img:draw(0, 0, frames[8 * 9 + math.floor(phase / 3)])
+        return
+    end
+    if phase == 0 then
+        img:draw(0, 0, frames[1 + dir])
+    else
+        img:draw(0, 0, frames[8 + dir * 8 + phase])
+    end
+end
+
+AddUnit {
+    name  = "civilian",
+    pMap  = sprite_renderer,
+    pInv  = pck_image("$(xcom)/units/civm.pck", 3),
+}
---
+++ extensions/unit-chameleon/ethereal.lua
@@ -0,0 +1,35 @@
+local frames = pck_image_set("$(xcom)/units/ethereal.pck", 0, 8 * 9 + 3)
+
+local function sprite_renderer(img, dir, phase, info)
+    if info.collapse then
+        img:draw(0, 0, frames[8 * 9 + math.floor(phase / 3)])
+        return
+    end
+
+    local function draw_body()
+        if phase == 0 then
+            img:draw(0, 0, frames[1 + dir])
+        else
+            img:draw(0, 0, frames[8 + dir * 8 + phase])
+        end
+    end
+
+    if not info.hand_object_image then draw_body() return end
+
+    -- depending on the direction we may have to draw weapon or the body first
+    if (dir < 1 or dir > 5) then
+        -- show a gun first, then overwrite it with a soldier body
+        img:draw(0, 0, info.hand_object_image)
+        draw_body()
+    else
+        -- show soldier body first, then overwrite it with a gun
+        draw_body()
+        img:draw(0, 0, info.hand_object_image)
+    end
+end
+
+AddUnit {
+    name  = "ethereal",
+    pMap  = sprite_renderer,
+    pInv  = pck_image("$(xcom)/units/ethereal.pck", 3),
+}
