14017a78dbda856cade3dafff803d542563363e4
serge <serge>
2005-08-15 04:47:47 +0000

    Added the latest version of new soldier unit skin from nappes
    git-svn-id: https://ufo2000.svn.sourceforge.net/svnroot/ufo2000/trunk@913 80a73507-52ba-0310-bdc1-b9b4a1856fab

--- extensions/unit-chameleon/soldier.lua
+++ extensions/unit-chameleon/soldier.lua
@@ -11,8 +11,8 @@
 local frames = {}
 
 for row = 7, 0, -1 do
-    for col = 0, 8 do
-        table.insert(frames, png_image_fragment("$(extension)/soldier.png", 
+    for col = 0, 20 do
+        table.insert(frames, png_image_fragment("$(extension)/soldier.png",
             1 + col * 33, 1 + math.mod(row + 4, 8) * 41, 32, 40))
     end
 end
@@ -29,14 +29,14 @@ end
 -- frames together and drawing them all on img object which is
 -- passed to this function as the first argument.
 --
--- img    - target sprite object, it supports 'draw' method, which 
---          accepts 3 arguments: x, y coordinates and a bitmap 
+-- img    - target sprite object, it supports 'draw' method, which
+--          accepts 3 arguments: x, y coordinates and a bitmap
 --          object (loaded from png or pck file for example)
 -- dir    - direction the soldier is facing (0, 1, 2, 3, 4, 5, 6, 7)
--- phase  - animation phase (0 - no animation, static image, 1-8 sequential 
+-- phase  - animation phase (0 - no animation, static image, 1-8 sequential
 --          frames)
 --
--- info   - table with some additional information, currently it can contain 
+-- info   - table with some additional information, currently it can contain
 --          the following properties:
 --            "hand_object_image" - contains bitmap with the weapon image
 --            "aimed_hand_object_image" - contains bitmap with the weapon image rotated 90 degrees
@@ -51,27 +51,44 @@ local function sprite_renderer(img, dir, phase, info)
         img:draw(0, 0, falling_frames[phase + 1])
         return
     end
-    -- ordinary image, no weapons in hands
+    
     if not info.hand_object_image then
-        img:draw(0, 0, frames[1 + dir * 9 + phase])
-        return
+        -- ordinary image, no weapons in hands
+        if not info.is_crouching then
+           img:draw(0, 0, frames[1 + dir * 21 + phase])
+           return
+        else
+        -- crouching, no weapons in hands
+           img:draw(0, 0, frames[20 + dir * 21])
+           return
+        end
     end
+
+
     -- crouching soldiers hold their weapons closer to the ground
-    local delta_y = 0
-    if info.is_crouching then delta_y = 4 end
+    local delta_y = -4
+    if info.is_crouching then delta_y = -1 end
     if (dir < 1 or dir > 5) then
         -- show a gun first, then overwrite it with a soldier body
         img:draw(0, delta_y, info.hand_object_image)
-        img:draw(0, 0, frames[1 + dir * 9 + phase])
+        if not info.is_crouching then
+           img:draw(0, 0, frames[11 + dir * 21 + phase])
+        else
+           img:draw(0, 0, frames[21 + dir * 21])
+        end
     else
         -- show soldier body first, then overwrite it with a gun
-        img:draw(0, 0, frames[1 + dir * 9 + phase])
+        if not info.is_crouching then
+           img:draw(0, 0, frames[11 + dir * 21 + phase])
+        else
+           img:draw(0, 0, frames[21 + dir * 21])
+        end
         img:draw(0, delta_y, info.hand_object_image)
     end
 end
 
 ------------------------------------------------------------------------------
--- Here comes unit description itself (only map sprite rendering function 
+-- Here comes unit description itself (only map sprite rendering function
 -- and inventory picture are defined right now)
 --
 -- Currenly AddUnit only adds appearances for a temporary unit
@@ -81,5 +98,11 @@ end
 AddUnit {
     name = "soldier",
     pMap  = sprite_renderer,
-    pInv  = png_image_fragment("$(extension)/soldier.png", 344, 0, 420 - 350, 150)
+    pInv = png_image_fragment("$(extension)/soldier_inv.png", 0, 0, 74, 106)
+}
+
+AddUnit {
+    name = "soldier(female)",
+    pMap  = sprite_renderer,
+    pInv = png_image_fragment("$(extension)/soldier_inv.png", 74, 0, 74, 106)
 }
