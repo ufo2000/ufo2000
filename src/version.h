e7349b7c387619e5fccc720e9ceb24283c7e605e
ssvb <ssvb>
2006-05-04 16:19:02 +0000

    * Added information about building experimental versions of the game * Fixed svn:eol-style property for gui.cpp and gui.h * Version bump
    git-svn-id: https://ufo2000.svn.sourceforge.net/svnroot/ufo2000/trunk@1031 80a73507-52ba-0310-bdc1-b9b4a1856fab

--- src/version.h
+++ src/version.h
@@ -23,7 +23,7 @@ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
 #define UFO_VERSION_TAG    "beta"
 #define UFO_VERSION_STRING "0.7"
-#define UFO_REVISION_NUMBER 1019
+#define UFO_REVISION_NUMBER 1031
 
 #ifndef UFO_SVNVERSION
 #define UFO_SVNVERSION "unknown"
